1294fb98a1811dfc9cf461909afaab54a3151c18
Gugulethu <mhlangagc@users.noreply.github.com>
2023-03-18 09:20:44 +0200

    Update EmailInboxInfo.swift
    @RSKuse Here is the updated change

--- EmailInboxAttachment/EmailInboxInfo.swift
+++ EmailInboxAttachment/EmailInboxInfo.swift
@@ -29,44 +29,12 @@ class EmailInboxInfo: UITableViewCell {
     
     lazy var detailLabel: UILabel = {
         let label = UILabel()
-        label.text = "Thanks man, please be on the look out for the pandadocs link for signatures. Regards Bandisa..."
+        label.text = "Situated just behind Coogee Beach, Courland House by Secret Gardens is a garden oasis home that has been discreetly tucked away to offer the owners the utmost privacy. Designed to make one feel calm, the family home of two landscape designers features many connections to outdoor spaces alongside warm interior choices. Intended to make each space shine separately, the external areas have been designed to be the hero of the family home and become spaces of indulgent tranquillity."
         label.textColor = .darkGray
+        label.numberOfLines = 2
         label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
-    }()
-    
-    lazy var backArrowImage: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "back_arrow_icon")
-        //imageView.layer.cornerRadius = 25
-        imageView.contentMode = .scaleAspectFill
-        //imageView.backgroundColor = .darkGray
-        imageView.clipsToBounds = true
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-    
-    lazy var nextArrowImage: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "arrow_next_right_icon")
-        //imageView.layer.cornerRadius = 25
-        imageView.contentMode = .scaleAspectFill
-        //imageView.backgroundColor = .darkGray
-        imageView.clipsToBounds = true
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-    
-    lazy var attachmentClipImage: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "attachment_paperclip_icon")
-        imageView.layer.cornerRadius = 25
-        imageView.contentMode = .scaleAspectFill
-        imageView.backgroundColor = .darkGray
-        imageView.clipsToBounds = true
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
     }()
     
     lazy var timestampLabel: UILabel = {
@@ -78,18 +46,11 @@ class EmailInboxInfo: UITableViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    lazy var labelStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [titleLabel, timestampLabel])
-        stackView.axis = .horizontal
-        stackView.distribution = .fillProportionally
-        stackView.spacing = 5.0
-        stackView.alignment = .leading
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
-    }()
     
-    lazy var userStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [labelStackView, timestampLabel])
+    // StackViews
+    lazy var titleTimeStampStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel,
+                                                       timestampLabel])
         stackView.axis = .horizontal
         stackView.distribution = .fillProportionally
         stackView.spacing = 10.0
@@ -98,8 +59,10 @@ class EmailInboxInfo: UITableViewCell {
         return stackView
     }()
     
-    lazy var sublabelStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [userStackView, subHeadingLabel, detailLabel])
+    lazy var subHeadingDetailStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleTimeStampStackView,
+                                                       subHeadingLabel,
+                                                       detailLabel])
         stackView.axis = .vertical
         stackView.distribution = .fillProportionally
         stackView.spacing = 5.0
@@ -107,31 +70,23 @@ class EmailInboxInfo: UITableViewCell {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: UITableViewCell.CellStyle.default, reuseIdentifier: reuseIdentifier)
         
-        addSubview(userStackView)
-        addSubview(sublabelStackView)
+        addSubview(subHeadingDetailStackView)
+        subHeadingDetailStackView.leftAnchor.constraint(equalTo: leftAnchor,
+                                                constant: 20).isActive = true
+        subHeadingDetailStackView.rightAnchor.constraint(equalTo: rightAnchor,
+                                                 constant: -20).isActive = true
+        subHeadingDetailStackView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         
+        titleTimeStampStackView.leftAnchor.constraint(equalTo: leftAnchor,
+                                                      constant: 20).isActive = true
+        titleTimeStampStackView.rightAnchor.constraint(equalTo: rightAnchor,
+                                                       constant: -20).isActive = true
         
-        accessoryType = UITableViewCell.AccessoryType.none
-        
-        userStackView.leftAnchor.constraint(equalTo: leftAnchor, constant: 20).isActive = true
-        userStackView.rightAnchor.constraint(equalTo: rightAnchor, constant: -16).isActive = true
-        userStackView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        
-        
-        backArrowImage.heightAnchor.constraint(equalToConstant: 20).isActive = true
-        backArrowImage.widthAnchor.constraint(equalToConstant: 20).isActive = true
-        
-        backArrowImage.leftAnchor.constraint(equalTo:titleLabel.rightAnchor, constant: 20).isActive = true
-        //userStackView.rightAnchor.constraint(equalTo: rightAnchor, constant: -16).isActive = true
-        //userStackView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        
-        
-        //nextArrowImage.heightAnchor.constraint(equalToConstant: 25).isActive = true
-        //nextArrowImage.widthAnchor.constraint(equalToConstant: 25).isActive = true
-        
+        // Add a width for the timestamp
         timestampLabel.widthAnchor.constraint(equalToConstant: 50).isActive = true
         
     }
